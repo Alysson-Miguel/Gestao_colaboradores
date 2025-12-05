4b04d40cabd0855b1134015736a229360b9c28ea
LukeRobs <lucassrobson06@gmail.com>
2025-12-04 22:06:55 -0300

    ajustes no import Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useContext } from "react";
 
 import Dashboard from "./pages/dashboard";
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Colaboradores from "./pages/colaboradores";
 import EmpresasPage from "./pages/empresas";
 import CargosPage from "./pages/cargos";
