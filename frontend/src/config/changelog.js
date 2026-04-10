2ed7fe6bcf74164558b27cda02c2cb5c5beff3fe
Alysson Miguel <Edicao.Dash@gmail.com>
2026-04-10 16:39:25 -0300

    1.11.1 agora vai boy

--- frontend/src/config/changelog.js
+++ frontend/src/config/changelog.js
@@ -2,7 +2,33 @@ const CHANGELOG = {
   version: "1.11.0",
   titulo: "Segurança e Isolamento de Dados por Estação",
   categorias: [
-
+    {
+      nome: "Isolamento de Dados",
+      itens: [
+        "Adicionado campo id_estacao às tabelas DW (dw_planejado e dw_real) para isolamento de dados por estação",
+        "Adicionado campo id_estacao às tabelas de Escala para segmentação por estação",
+        "Adicionado campo id_estacao às tabelas de Turno, Cargo e Empresa",
+        "Adicionado campo id_estacao à tabela de Desligamento",
+        "Adicionado campo id_estacao à tabela de Setor",
+        "Implementado relacionamento entre usuários e estações (user_estacao)",
+        "Criado perfil Alta Gestão com acesso a todas as estações"
+      ]
+    },
+    {
+      nome: "Configurações",
+      itens: [
+        "Adicionado suporte a configuração de planilhas Google Sheets por estação",
+        "Adicionado configuração de planilha de presença por estação (sheets_presenca)"
+      ]
+    },
+    {
+      nome: "Melhorias",
+      itens: [
+        "Corrigido tipo de data do campo data_referencia na tabela de sugestões",
+        "Criados índices para otimizar consultas por estação",
+        "Implementadas constraints de unicidade considerando estação"
+      ]
+    }
   ],
 };
 
